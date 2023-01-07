fa62108051eade240ad31d4c936fd336f59220e3
shannyuu <97235376+shannyuu@users.noreply.github.com>
2023-01-08 03:10:57 +0800

    left out models

---
+++ kampong-goods/Models/Voucher.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kampong_goods.Models
+{
+    public class Voucher
+    {
+        [Required, MaxLength(8)]
+        public string VoucherId { get; set; } = string.Empty;
+
+        [Required, MinLength(3, ErrorMessage = "Enter at least 3 characters."), MaxLength(30)]
+        public string VoucherName { get; set; } = string.Empty;
+
+        [Required, Range(1, 1000)]
+        public decimal VoucherDiscAmt { get; set; }
+
+        [Required, Range(0, 100)]
+        public decimal VoucherMinSpend { get; set; }
+
+        [Required, MaxLength(8)]
+        public string VoucherCode { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Expiry")]
+        public DateTime VoucherExpDate { get; set; } = new DateTime(DateTime.Now.Year - 18, 1, 1);
+
+    }
+}
